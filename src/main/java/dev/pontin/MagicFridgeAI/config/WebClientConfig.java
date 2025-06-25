ef79ff3b09b91b5cf86bafac334f16d307c0d0bf
Pontinn <leo.pontin2@gmail.com>
2025-06-25 02:27:21 -0300

    feat: create a new bean for use to create images

--- src/main/java/dev/pontin/MagicFridgeAI/config/WebClientConfig.java
+++ src/main/java/dev/pontin/MagicFridgeAI/config/WebClientConfig.java
@@ -1,21 +1,31 @@
 package dev.pontin.MagicFridgeAI.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
-    @Value("${chatgpt.api.url:https://api.openai.com/v1/responses}")
+    @Value("${chatgpt.api.url:https://api.openai.com/v1}")
     private String chatGptApiUrl;
 
-    @Bean
-    WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl(chatGptApiUrl)
+    @Primary
+    @Bean("recipeWebClient")
+    @Qualifier("recipeApi")
+    WebClient webClientRecipe(WebClient.Builder builder) {
+        return builder.baseUrl(chatGptApiUrl + "/responses")
                 .build();
     }
 
+    @Bean("imageWebClient")
+    @Qualifier("imageApi")
+    WebClient webClientImage(WebClient.Builder builder) {
+        return builder.baseUrl(chatGptApiUrl + "/images/generations")
+                .build();
+    }
 
 }
