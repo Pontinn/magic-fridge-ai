8d4fef3b7f4fd91a4d3f4d4b252f558bb7ae599e
Pontinn <leo.pontin2@gmail.com>
2025-06-25 02:00:06 -0300

    feat: add route to create recipe

---
+++ src/main/java/dev/pontin/MagicFridgeAI/controller/RecipeController.java
@@ -0,0 +1,33 @@
+package dev.pontin.MagicFridgeAI.controller;
+
+import dev.pontin.MagicFridgeAI.model.FoodItemModel;
+import dev.pontin.MagicFridgeAI.service.ChatGptService;
+import dev.pontin.MagicFridgeAI.service.FoodItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/receita")
+public class RecipeController {
+
+    private final FoodItemService foodItemService;
+    private final ChatGptService gptService;
+
+    public RecipeController(FoodItemService foodItemService, ChatGptService gptService) {
+        this.foodItemService = foodItemService;
+        this.gptService = gptService;
+    }
+
+    @GetMapping("/gerar")
+    public Mono<ResponseEntity<String>> generateRecipe() {
+        List<FoodItemModel> foodList = foodItemService.listar();
+        return gptService.generateRecipe(foodList)
+                .map(response -> ResponseEntity.ok(response))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
+    }
+}
